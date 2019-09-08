314884570016149c01646f3247f89b764397e4b9
John Vehikite <johncookv@gmail.com>
2019-09-07 23:30:20 -0700

    Revert last update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
+import Layout from "../components/Layout"
 
 export default () => (
-  <Container>
-    <Row>
-      <Col>
-        <h1>Homepage</h1>
-      </Col>
-    </Row>
-  </Container>
+  <Layout>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Homepage</h1>
+        </Col>
+      </Row>
+    </Container>
+  </Layout>
 )
--- src/pages/support-and-resources.jsx
+++ src/pages/support-and-resources.jsx
@@ -1,12 +1,15 @@
 import React from "react"
+import Layout from "../components/Layout"
 import { Container, Row, Col } from "react-bootstrap"
 
 export default () => (
-  <Container>
-    <Row>
-      <Col>
-        <h1>Support and Resources Page</h1>
-      </Col>
-    </Row>
-  </Container>
+  <Layout>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Support and Resources Page</h1>
+        </Col>
+      </Row>
+    </Container>
+  </Layout>
 )
