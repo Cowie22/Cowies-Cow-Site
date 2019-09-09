815b9dfa39e0888210ac7fb390e486bc1dff992b
John Vehikite <johncookv@gmail.com>
2019-09-09 09:05:36 -0700

    Fix deploy bugs

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,18 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
+import { useStateValue } from "../contexts/state"
 
-import HeaderSwitcher from "./header/HeaderSwitcher"
+import Header from "./header/Header"
+import HeaderNoIndication from "./header/HeaderNoIndication"
 import Footer from "./footer/Footer"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import "../pages/index.scss"
-// Import font-awesome
-// Sources:
-// https://github.com/gatsbyjs/gatsby/issues/6357#issuecomment-490972478 - source of code below
-// https://github.com/FortAwesome/react-fontawesome#build-a-library-to-reference-icons-throughout-your-app-more-conveniently
-// import { library } from "@fortawesome/fontawesome-svg-core"
-// import brand icons
-// import { fab } from "@fortawesome/free-brands-svg-icons"
-// import pro icons
-// import { faTimesCircle, faAngleRight } from "@fortawesome/pro-light-svg-icons"
-// then add icons to the library
-// library.add(faTimesCircle, faAngleRight)
 
 const Layout = ({ children }) => {
+  const [{ isIndicationVisible }] = useStateValue()
   return (
     <div className="layout">
       <Helmet>
@@ -28,7 +20,7 @@ const Layout = ({ children }) => {
         <title>Evoke Giant Starter</title>
         <meta name="description" content="Evoke Giant Starter description" />
       </Helmet>
-      <HeaderSwitcher />
+      {isIndicationVisible ? <Header /> : <HeaderNoIndication />}
       <div className="main">{children}</div>
       <Footer />
     </div>
