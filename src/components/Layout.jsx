c5f64ec7ca4ae0d23ea5a498338a61fd26a2d591
John Vehikite <johncookv@gmail.com>
2019-09-11 14:01:25 -0700

    Add AppWrapper back to gatsby-browser Still causing issues during build, but runs fine locally

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,62 +1,25 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
-import { StickyContainer } from "react-sticky"
+import { useStateValue } from "../contexts/state"
 
-import { StateProvider } from "../contexts/state"
-import HeaderSwitcher from "./header/HeaderSwitcher"
+import Header from "./header/Header"
+import HeaderNoIndication from "./header/HeaderNoIndication"
 import Footer from "./footer/Footer"
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "@fortawesome/fontawesome-svg-core/styles.css"
-import "../pages/index.scss"
-// Import font-awesome
-// Sources:
-// https://github.com/gatsbyjs/gatsby/issues/6357#issuecomment-490972478 - source of code below
-// https://github.com/FortAwesome/react-fontawesome#build-a-library-to-reference-icons-throughout-your-app-more-conveniently
-// import { library } from "@fortawesome/fontawesome-svg-core"
-// import brand icons
-// import { fab } from "@fortawesome/free-brands-svg-icons"
-// import pro icons
-// import { faTimesCircle, faAngleRight } from "@fortawesome/pro-light-svg-icons"
-// then add icons to the library
-// library.add(faTimesCircle, faAngleRight)
-
 const Layout = ({ children }) => {
-  const initialState = {
-    isIndicationVisible: true,
-  }
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "hideIndication":
-        return {
-          ...state,
-          isIndicationVisible: false,
-        }
-
-      default:
-        return state
-    }
-  }
+  const [{ isIndicationVisible }] = useStateValue()
   return (
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <StickyContainer>
-        <div className="layout">
-          <Helmet>
-            <meta charSet="utf-8" />
-            <title>Evoke Giant Starter</title>
-            <meta
-              name="description"
-              content="Evoke Giant Starter description"
-            />
-          </Helmet>
-          <HeaderSwitcher />
-          <div className="main">{children}</div>
-          <Footer />
-        </div>
-      </StickyContainer>
-    </StateProvider>
+    <div className="layout">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Evoke Giant Starter</title>
+        <meta name="description" content="Evoke Giant Starter description" />
+      </Helmet>
+      {isIndicationVisible ? <Header /> : <HeaderNoIndication />}
+      <div className="main">{children}</div>
+      <Footer />
+    </div>
   )
 }
 
