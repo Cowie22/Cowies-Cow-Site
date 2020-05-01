49e50d25149792fe40ae3692faa5e5ca99cd0e79
Wilson Chun <wchun108@gmail.com>
2020-05-01 14:56:01 -0700

    Refactor to useContext

--- src/components/exitramp/ExitRamp.jsx
+++ src/components/exitramp/ExitRamp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { AppContext } from "../../contexts/state"
 
@@ -10,90 +10,79 @@ import externalLinkGreen from "../../assets/images/externalLinkGreen.svg"
 
 export default props => {
   const [hovered, handleHovered] = useState()
+  const state = useContext(AppContext)
+
+  useEffect(() => {
+    if (state.isExitRampOpen) {
+      document.querySelector("body").classList.add("exit-ramp-opened")
+    } else {
+      document.querySelector("body").classList.remove("exit-ramp-opened")
+    }
+  })
 
   return (
-    <AppContext.Consumer>
-      {context => {
-        const { handleIsExitRampOpen, isExitRampOpen, externalUrl } = context
-
-        if (isExitRampOpen) {
-          document.querySelector("body").classList.add("exit-ramp-opened")
-        } else {
-          document.querySelector("body").classList.remove("exit-ramp-opened")
-        }
-
-        return (
-          <section className={isExitRampOpen ? "overlay" : "overlay hidden"}>
-            <Container>
+    <section className={state.isExitRampOpen ? "overlay" : "overlay hidden"}>
+      <Container>
+        <Row>
+          <Col lg={{ span: 10, offset: 1 }}>
+            <div className="exit-ramp text-center">
+              <img src={Logo} width={170} />
+              <p>
+                <strong>You are now leaving evokegiant.com.</strong>
+              </p>
               <Row>
-                <Col lg={{ span: 10, offset: 1 }}>
-                  <div className="exit-ramp text-center">
-                    <img src={Logo} width={170} />
-                    <p>
-                      <strong>You are now leaving evokegiant.com.</strong>
-                    </p>
-                    <Row>
-                      <Col
-                        lg={{ span: 6, order: 1 }}
-                        xs={{ span: 12, order: 12 }}
-                        className="link-wrapper"
-                      >
-                        <button
-                          className="link text-center"
-                          onClick={() => handleIsExitRampOpen()}
-                          onMouseEnter={() => handleHovered(1)}
-                          onMouseLeave={() => handleHovered(0)}
-                        >
-                          <span>Return to evokegiant.com</span>
-                          <div>
-                            <img
-                              src={
-                                hovered === 1
-                                  ? arrowRightWhite
-                                  : arrowRightGreen
-                              }
-                              style={{ height: "13px", marginBottom: "1px" }}
-                            />
-                          </div>
-                        </button>
-                      </Col>
-                      <Col
-                        lg={{ span: 6, order: 12 }}
-                        xs={{ span: 12, order: 1 }}
-                        className="link-wrapper"
-                      >
-                        <a
-                          className="link text-center"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          href={externalUrl}
-                          onClick={() => handleIsExitRampOpen(externalUrl)}
-                          onMouseEnter={() => handleHovered(2)}
-                          onMouseLeave={() => handleHovered(0)}
-                        >
-                          <span style={{ paddingTop: "1px" }}>
-                            Continue to site
-                          </span>
-                          <div>
-                            <img
-                              src={
-                                hovered === 2
-                                  ? externalLinkGreen
-                                  : externalLinkWhite
-                              }
-                              style={{ height: "13px", marginBottom: "1px" }}
-                            />
-                          </div>
-                        </a>
-                      </Col>
-                    </Row>
-                  </div>
+                <Col
+                  lg={{ span: 6, order: 1 }}
+                  xs={{ span: 12, order: 12 }}
+                  className="link-wrapper"
+                >
+                  <button
+                    className="link text-center"
+                    onClick={() => state.handleIsExitRampOpen()}
+                    onMouseEnter={() => handleHovered(1)}
+                    onMouseLeave={() => handleHovered(0)}
+                  >
+                    <span>Return to evokegiant.com</span>
+                    <div>
+                      <img
+                        src={hovered === 1 ? arrowRightWhite : arrowRightGreen}
+                        style={{ height: "13px", marginBottom: "1px" }}
+                      />
+                    </div>
+                  </button>
+                </Col>
+                <Col
+                  lg={{ span: 6, order: 12 }}
+                  xs={{ span: 12, order: 1 }}
+                  className="link-wrapper"
+                >
+                  <a
+                    className="link text-center"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={state.externalUrl}
+                    onClick={() =>
+                      state.handleIsExitRampOpen(state.externalUrl)
+                    }
+                    onMouseEnter={() => handleHovered(2)}
+                    onMouseLeave={() => handleHovered(0)}
+                  >
+                    <span style={{ paddingTop: "1px" }}>Continue to site</span>
+                    <div>
+                      <img
+                        src={
+                          hovered === 2 ? externalLinkGreen : externalLinkWhite
+                        }
+                        style={{ height: "13px", marginBottom: "1px" }}
+                      />
+                    </div>
+                  </a>
                 </Col>
               </Row>
-            </Container>
-          </section>
-        )
-      }}
-    </AppContext.Consumer>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
--- src/components/exitramp/ExternalLink.jsx
+++ src/components/exitramp/ExternalLink.jsx
@@ -1,29 +1,23 @@
-import React from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
 import { AppContext } from "../../contexts/state"
 
 const ExternalLink = ({ children, externalUrl, className, id }) => {
+  const state = useContext(AppContext)
   return (
-    <AppContext.Consumer>
-      {context => {
-        const { handleIsExitRampOpen } = context
-        return (
-          <a
-            href={externalUrl}
-            id={id}
-            className={className}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={e => {
-              e.preventDefault()
-              handleIsExitRampOpen(externalUrl)
-            }}
-          >
-            {children}
-          </a>
-        )
+    <a
+      href={externalUrl}
+      id={id}
+      className={className}
+      target="_blank"
+      rel="noopener noreferrer"
+      onClick={e => {
+        e.preventDefault()
+        state.handleIsExitRampOpen(externalUrl)
       }}
-    </AppContext.Consumer>
+    >
+      {children}
+    </a>
   )
 }
 
