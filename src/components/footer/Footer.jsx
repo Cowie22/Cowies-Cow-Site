f8c1b3ad7f4d53a3e3c56a061c8e2b554de5c9b7
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-04-26 20:27:14 -0700

    Adds mobile styles to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,11 +9,11 @@ export default () => (
   <footer>
     <Container>
       <Row>
-        <Col lg={{ span: 12, order: 1}}>
-          <h6>
+        <Col lg={{ span: 5, offset: 0}} xs={{span: 6, offset: 3}}>
+          <h6 className='footer-top-title'>
             For US Residents Only
           </h6>
-          <nav>
+          <nav className='footer-nav-container'>
             <ul>
               <li>
                 <Link to="#">Contact Us</Link>
@@ -32,7 +32,7 @@ export default () => (
         </Col>
       </Row>
       <Row>
-        <Col lg={{span: 6, offset: 0}}>
+        <Col lg={{span: 6, offset: 0, order: 1}} xs={{span: 10, offset: 1, order: 2}}>
           <h6 className='footer-text'>
             Myfembree® and its associated logo are trademarks of Myovant Sciences GmbH. All other
             trademarks are the property of their respective owners.
@@ -41,7 +41,7 @@ export default () => (
             ©2021 Myovant Sciences GmbH and Pfizer Inc. All rights reserved. PP-US-REL-CT-2100061 06/21
           </h6>
         </Col>
-        <Col lg={{span: 4, offset: 2}}>
+        <Col lg={{span: 4, offset: 2, order: 2}} xs={{span: 10, offset: 1, order: 1}}>
           <div className='footer-logo-container'>
             <img src={footerLogo} />
           </div>
