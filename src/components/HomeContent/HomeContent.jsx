2d1f7786a1f6fe9829ff73201df3abdcce925a7b
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-04-28 18:47:24 -0700

    Adds home hero card

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -23,28 +23,45 @@ class HomeContent extends React.Component {
         <section className='home-container' title='Woman looking hopeful'>
           <Container>
             <Row>
-              <Col lg={{span: 6, offset: 6}} xs={{span: 11, offset: 1}}>
-                <h1 className='white'>
-                  Myfembree–a new treatment for symptoms of uterine fibroids
+              <Col lg={{span: 9, offset: 0}} xs={{span: 11, offset: 1}}>
+                <h1 className='medium-green'>
+                  NOW APPROVED
                 </h1>
-                <h3 className='home-sub-title medium-green'>
-                  Talk to your doctor today <br className='d-xs-block d-md-none'></br> about Myfembree
+                <h2 className='home-sub-title white'>
+                  One small pill, once a day: <br /> Myfembree for heavy menstrual bleeding
+                  associated with uterine fibroids
+                </h2>
+                <h3 className='white'>
+                  Set your calendars—available for use June 17, 2021
                 </h3>
-                <p className='white'>
-                  Myfembree is a prescription medicine that is used in the treatment of heavy
-                  menstrual bleeding associated with uterine fibroids.
-                </p>
-                <Row>
-                  <Col lg={4} xs={{span: 6}}>
-                    <div className='cta-btn-container home-btn-container'>
-                      <a>
-                        <button className='cta-btn transparent-btn'>
-                          Learn More
-                        </button>
-                      </a>
-                    </div>
-                  </Col>
-                </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={3}>
+                <div className='cta-btn-container home-btn-container'>
+                  <a>
+                    <button className='cta-btn white-btn'>
+                      View Prescribing Information
+                    </button>
+                  </a>
+                </div>
+              </Col>
+              <Col lg={3}>
+                <div className='cta-btn-container home-btn-container'>
+                  <a>
+                    <button className='cta-btn transparent-btn'>
+                      View the Press Release
+                    </button>
+                  </a>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h3 className='white'>
+                  To find out more about the Myfembree Support Program, <br /> call 1-833-MYFEMBREE (1-833-693-3627),
+                  8 AM-8 PM ET, Monday-Friday.
+                </h3>
               </Col>
             </Row>
           </Container>
