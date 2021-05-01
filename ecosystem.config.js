a636050caf3c3768916e344198dd6f3302321b2b
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-04-30 17:03:25 -0700

    Updates to ecosystem for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@
  * Project configuration section
  */
 
-var project_name = "myovant-hcp-gatsby-splash"
+var project_name = "myovant-hcp-gatsby-deluxe"
 var client_name = "myovant"
 var git_repo = "git@github.com:GiantAgency/Myovant-HCP-Gatsby.git"
-var git_branch = "splash"
+var git_branch = "microsite"
 var fontawesome =
   "npm config set @fortawesome:registry https://npm.fontawesome.com/EEFD84F8-907F-4B20-82D9-DC20387F3808"
 var fortawesome_cmd =
