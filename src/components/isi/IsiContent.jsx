9eeab006c67f52087837c8c6cad588b5dcd6b698
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-05-13 13:01:26 -0700

    Adds loreum ipsum to isi

--- src/components/isi/IsiContent.jsx
+++ src/components/isi/IsiContent.jsx
@@ -32,237 +32,237 @@ class IsiContent extends React.Component {
             myocardial infarction, especially in women at increased risk for these events.
           </p>
         </div>
-        <p className='purple extra-bold isi-header-text'>
-          CONTRAINDICATIONS
-        </p>
         <p>
-          MYFEMBREE is contraindicated in women with current or a history of thrombotic or thromboembolic
-          disorders and in women at increased risk for these events, including women over 35 years of age
-          who smoke or women with uncontrolled hypertension.
+          Lorem Ipsum is simply dummy text of the printing and typesetting
         </p>
         <ul className='purple-ul'>
-          <li>
-            Myfembree may increase your chances of heart attack, stroke, or blood clots, especially
-            if you are over 35 years of age and smoke, have uncontrolled high blood pressure, high
-            cholesterol, diabetes, or are obese. <strong>Stop taking Myfembree and call your healthcare
-            provider right away or go to the nearest hospital emergency room right away if you have:</strong>
+          <li className='purple extra-bold'>
+            Lorem Ipsum is simply dummy
           </li>
           <ul className='purple-ul-dashed'>
             <li>
-              Leg pain or swelling that will not go away
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
             </li>
-            <li>
-              Sudden shortness of breath
-            </li>
-            <li>
-              Double vision, bulging of the eyes, or sudden blindness (partial or complete)
-            </li>
-            <ul className='purple-ul'>
+            <ul className='purple-ul-square'>
               <li>
-                Pain or pressure in your chest, arm, or jaw
+                Lorem Ipsum is simply dummy
               </li>
               <li>
-                Sudden, severe headache unlike your usual headaches
+                Lorem Ipsum is simply dummy
               </li>
               <li>
-                Weakness or numbness in an arm or leg, or trouble speaking
+                Lorem Ipsum is simply dummy
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy
               </li>
             </ul>
           </ul>
           <li className='purple extra-bold'>
-            Bone Loss (Decreased Bone Mineral Density [BMD])
+           Lorem Ipsum is simply dummy text of the printing and
           </li>
           <ul className='purple-ul-dashed'>
             <li>
-              While taking Myfembree, your estrogen levels may be low. Low estrogen levels can lead to BMD loss.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer
             </li>
             <li>
-              If you have bone loss on Myfembree, your BMD may improve after you stop taking Myfembree,
-              but complete recovery may not occur. It is unknown if these BMD changes could increase
-              your risk for broken bones as you age. For this reason, you should not take Myfembree for
-              more than 24 months.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
             </li>
             <li>
-              Your healthcare provider may order an X-ray test called a DXA scan to check your bone mineral
-              density when you start taking Myfembree and periodically after you start.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer
             </li>
             <li>
-              Your doctor may advise you to take vitamin D and calcium supplements as part of a healthy lifestyle.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
             </li>
           </ul>
           <li className='purple extra-bold'>
-            Effects on Pregnancy
+           Lorem Ipsum is simply dummy text of the printing and
           </li>
           <ul className='purple-ul-dashed'>
             <li>
-              Do not take Myfembree if you are pregnant or trying to become pregnant, as it may increase the
-              risk of early pregnancy loss.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer
             </li>
             <li>
-              If you think you may be pregnant, stop taking Myfembree right away and call your HCP.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
             </li>
             <li>
-              Myfembree can decrease your menstrual bleeding or result in no menstrual bleeding at all,
-              making it hard to know if you are pregnant. Watch for other pregnancy signs like breast
-              tenderness, weight gain, and nausea.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer
             </li>
             <li>
-              Myfembree does not prevent pregnancy. You will need to use effective methods of birth control
-              while taking Myfembree and for 1 week after you stop taking Myfembree. Examples of effective
-              methods can include condoms or spermicide, which do not contain hormones.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
+            </li>
+          </ul>
+          <li className='purple extra-bold'>
+           Lorem Ipsum is simply dummy text of the printing and
+          </li>
+          <ul className='purple-ul-dashed'>
+            <li>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer
             </li>
             <li>
-              Talk to your HCP about which birth control to use during treatment with Myfembree. Your
-              HCP may change the birth control you are on before you start taking Myfembree.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
+            </li>
+            <li>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer
+            </li>
+            <li>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+              but also the leap int
             </li>
           </ul>
         </ul>
         <p className='purple extra-bold'>
-          What is Myfembree?
-        </p>
-        <p>
-          Myfembree is a prescription medicine used to control heavy menstrual bleeding in premenopausal women
-          (before “change of life” or menopause) with uterine fibroids. It is not known if Myfembree is safe and
-          effective in children under 18 years of age.
-        </p>
-        <p className='purple extra-bold'>
-          Do not take Myfembree if you:
+          Lorem Ipsum is simply dummy text of the
         </p>
         <ul className='purple-ul'>
           <li>
-            Have or have had:
+            Lorem Ipsum is simply dummy text
           </li>
           <ul className='purple-ul-dashed'>
             <li>
-              A stroke or heart attack
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              A problem that makes your blood clot more than normal
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              Blood circulation disorder
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              Certain heart valve problems or heart rhythm abnormalities that can cause blood clots to form in the heart
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              Blood clots in your legs (deep vein thrombosis), lungs (pulmonary embolism), or eyes (retinal thrombosis)
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              High blood pressure not well controlled by medicine
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              Diabetes with kidney, eye, nerve, or blood vessel damage
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              Certain kinds of headaches with numbness, weakness, or changes in vision, or have migraine
-              headaches with aura if you are over age 35
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              Breast cancer or any cancer that is sensitive to female hormones
-            </li>
-            <li>
-              Osteoporosis
-            </li>
-            <li>
-              Unexplained vaginal bleeding that has not been diagnosed
-            </li>
-            <li>
-              Liver problems including liver disease
-            </li>
-            <li>
-              Smoke and are over 35 years old
+            Lorem Ipsum is simply dummy text
             </li>
           </ul>
           <li>
-            are taking medicines known as OATP1B1 inhibitors that are known or expected to significantly
-            increase the blood levels of elagolix (an ingredient in Myfembree). Ask your healthcare provider
-            if you are not sure if you are taking this type of medicine.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
           <li>
-            have had a serious allergic reaction to elagolix, estradiol, norethindrone acetate, or any of the
-            ingredients in Myfembree. Ask your healthcare provider if you are not sure.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
           <li>
-            FD&C Yellow No.5 (tartrazine) is an ingredient in Myfembree which may cause an allergic type reaction
-            such as bronchial asthma in some patients who are also allergic to aspirin. See the end of this
-            Medication Guide for a complete list of ingredients in Myfembree.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
+          </li>
+          <li>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
         </ul>
         <p className='purple extra-bold'>
-          Before you take Myfembree, tell your healthcare provider about all of your medical conditions, including if you:
+          Lorem Ipsum is simply dummy text of the
         </p>
         <ul className='purple-ul'>
           <li>
-            Have or have had:
+            Lorem Ipsum is simply dummy text
           </li>
           <ul className='purple-ul-dashed'>
             <li>
-              broken bones or other conditions that may cause bone problems.
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              depression, mood swings, or suicidal thoughts or behavior.
+            Lorem Ipsum is simply dummy text
             </li>
             <li>
-              yellowing of the skin or eyes (jaundice) or jaundice caused by pregnancy (cholestasis of pregnancy).
+            Lorem Ipsum is simply dummy text
+            </li>
+            <li>
+            Lorem Ipsum is simply dummy text
+            </li>
+            <li>
+            Lorem Ipsum is simply dummy text
+            </li>
+            <li>
+            Lorem Ipsum is simply dummy text
+            </li>
+            <li>
+            Lorem Ipsum is simply dummy text
+            </li>
+            <li>
+            Lorem Ipsum is simply dummy text
+            </li>
+            <li>
+            Lorem Ipsum is simply dummy text
             </li>
           </ul>
           <li>
-            are scheduled for surgery. Myfembree may increase your risk of blood clots after surgery. Your doctor
-            may advise you to stop taking Myfembree before you have surgery. If this happens, talk to your
-            healthcare provider about when to restart Myfembree after surgery.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
           <li>
-            are pregnant or think you may be pregnant.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
           <li>
-            are breastfeeding. It is not known if Myfembree can pass into your breastmilk. Talk to your
-            healthcare provider about the best way to feed your baby if you take Myfembree.
-          </li>
-        </ul>
-        <p>
-          <strong>Tell your healthcare provider about all the medicines you take</strong>, including prescription and
-          over-the counter medicines, vitamins, and herbal supplements. Women on thyroid or cortisol replacement
-          therapy may need increased doses of the hormone. Know the medicines you take. Keep a list of your
-          medicines with you to show to your healthcare provider and pharmacist when you get a new medicine.
-        </p>
-        <p className='purple extra-bold'>
-          How should I take Myfembree?
-        </p>
-        <ul className='purple-ul'>
-          <li>
-            Take Myfembree exactly as your healthcare provider tells you to take it.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
           <li>
-            Your healthcare provider will give you a pregnancy test before you start taking Myfembree or will have
-            you start taking Myfembree within 7 days after you start your period.
-          </li>
-          <li>
-            Take 1 white and yellow Myfembree capsule in the morning and 1 white and light blue Myfembree
-            capsule in the evening each day.
-          </li>
-          <li>
-            Take Myfembree at about the same time each morning and evening with or without food.
-          </li>
-          <li>
-            If you take too much Myfembree, call your healthcare provider or go to the nearest hospital
-            emergency room right away.
-          </li>
-        </ul>
-        <p className='purple extra-bold'>
-          If you miss a dose of Myfembree?
-        </p>
-        <ul className='purple-ul'>
-          <li>
-            Take the missed dose within 4 hours of the time that it was supposed to be taken. Then take the
-            next dose at the usual time.
-          </li>
-          <li>
-            If more than 4 hours have passed since you usually take the morning or evening dose, skip
-            the missed dose. Take your next dose at the usual time.
-          </li>
-          <li>
-            <strong>Do not</strong> take 2 doses to make up for the missed dose.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+            but also the leap int
           </li>
         </ul>
         <p className='lower-isi-callout'>
