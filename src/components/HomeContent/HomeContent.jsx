61b6b1790921897c53653deac037807ab27b89f5
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-05-13 12:57:07 -0700

    Adjustment for mobile

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -90,7 +90,7 @@ class HomeContent extends React.Component {
             </Row>
             <Row>
               <Col lg={{span: 9, offset: 0}} xs={{span: 11, offset: 0}}>
-                <h4 className='white extra-bold'>
+                <h4 className='white extra-bold home-bottom-text'>
                   Available on June 17, 2021
                 </h4>
               </Col>
