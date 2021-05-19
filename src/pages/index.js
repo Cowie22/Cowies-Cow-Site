b3957648297f690f0ed41154b5d8d6f51e952c12
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-05-19 11:54:32 -0700

    Adds custom 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import AppProvider, { AppContext } from '../contexts/state'
 
 import HomeContent from '../components/HomeContent/HomeContent'
