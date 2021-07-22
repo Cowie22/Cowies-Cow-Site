0f3c7c2674494a979d64f90b198ae2ecdd70e26e
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-07-21 22:03:54 -0700

    Adds another table

--- src/components/CircleGraphCallout/CircleGraphCallout.jsx
+++ src/components/CircleGraphCallout/CircleGraphCallout.jsx
@@ -6,33 +6,31 @@ const CircleGraphCallout = (props) => {
   const { data } = props;
   const { title, subTitle, circleIconOne, circleIconTwo, content } = data[0];
   return (
-    <Col lg={{span: 12, offset: 0}}>
-      <div className='circle-graph-callout-container'>
-        {title}
-        <Row>
-          <Col lg={{span: 10, offset: 1}}>
-            {subTitle}
-            <Row>
-              <Col lg={{span: 3}}>
-                <div className='circle-icon-container'>
-                  <img src={circleIconOne} alt='' />
-                </div>
-              </Col>
-              <Col lg={{span: 3}}>
-                <div className='circle-icon-container'>
-                  <img src={circleIconTwo} alt='' />
-                </div>
-              </Col>
-              <Col lg={{span: 6}}>
-                <div className='circle-graph-content-container'>
-                  {content}
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </div>
-    </Col>
+    <div className='circle-graph-callout-container'>
+      {title}
+      <Row>
+        <Col lg={{span: 10, offset: 1}}>
+          {subTitle}
+          <Row>
+            <Col lg={{span: 3}}>
+              <div className='circle-icon-container'>
+                <img src={circleIconOne} alt='' />
+              </div>
+            </Col>
+            <Col lg={{span: 3}}>
+              <div className='circle-icon-container'>
+                <img src={circleIconTwo} alt='' />
+              </div>
+            </Col>
+            <Col lg={{span: 6}}>
+              <div className='circle-graph-content-container'>
+                {content}
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
--- src/components/SafetyTabs/AdverseEvents/AdverseEvents.jsx
+++ src/components/SafetyTabs/AdverseEvents/AdverseEvents.jsx
@@ -16,15 +16,105 @@ const AdverseEvents = (props) => {
             <CircleGraphCallout data={CircleGraphCalloutData1} />
             <CircleGraphCallout data={CircleGraphCalloutData2} />
           </div>
+          <h2 className='blue'>
+            Most common adverse events seen in Phase 3 clinical trials
+          </h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{span: 10, offset: 1}}>
+          <table class='interior-table'>
+            <tbody>
+              <tr>
+                <th>
+
+                </th>
+                <th>
+                  Myfembree (n = 254) %
+                </th>
+                <th>
+                  Placebo (n = 256) %
+                </th>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    Hot flush, hyperhidrosis, or night sweats
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    10.6
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    6.6
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    Abnormal uterine bleeding*
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    6.3
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    1.2
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    Alopecia<sup>†</sup>
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    3.5
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    0.8
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    Libido decreased<sup>‡</sup>
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    3.1
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    0.4
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </Col>
       </Row>
       <Row>
         <Col lg={{span: 9, offset: 1}}>
-          <div className='footnote-container'>
+          {/* <div className='footnote-container'>
             <h6>
               *Iron therapy was required for women with Hgb ≥8 g/dL and ≤10 g/dL.
             </h6>
-          </div>
+          </div> */}
         </Col>
       </Row>
     </div>
