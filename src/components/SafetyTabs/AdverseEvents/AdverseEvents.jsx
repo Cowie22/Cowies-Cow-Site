79a65a36c35fa51cce17d2e79d4fcb534a3d1946
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-07-21 22:27:06 -0700

    Adds another tab of content

--- src/components/SafetyTabs/AdverseEvents/AdverseEvents.jsx
+++ src/components/SafetyTabs/AdverseEvents/AdverseEvents.jsx
@@ -109,6 +109,49 @@ const AdverseEvents = (props) => {
         </Col>
       </Row>
       <Row>
+        <Col lg={{span: 10, offset: 1}}>
+          <div className='lower-content-container'>
+            <p>
+              Adverse events occurring in ≥3% of women treated with Myfembree and at a greater
+              incidence than placebo.
+            </p>
+            <div className='footnote-container'>
+              <h6>
+                *Includes menorrhagia, metrorrhagia, vaginal hemorrhage, polymenorrhea, and irregular menstruation.
+              </h6>
+              <h6>
+                †Alopecia includes alopecia, hair loss, and hair thinning, in no specific pattern.
+              </h6>
+              <h6>
+                ‡Includes decreased libido and loss of libido.
+              </h6>
+            </div>
+            <ul className='purple-ul'>
+              <li>
+                <strong>Hypertension:</strong> In 1 of the 2 clinical trials (LIBERTY 1), more women
+                experienced new or worsening hypertension with Myfembree than with
+                placebo (7.0% vs 0.8%, respectively)<sup>1</sup>
+              </li>
+              <li>
+                <strong>Alopecia:</strong> Three of the 11 total affected women treated with Myfembree
+                in LIBERTY 1 and 2 and the LIBERTY open-label extension study reported moderate
+                alopecia; the other 8 women reported mild alopecia. For 1 patient, who was in
+                the extension study, alopecia was a reason for discontinuing treatment<sup>1</sup>
+              </li>
+              <li>
+                <strong>Abnormal uterine bleeding:</strong> The most common adverse reaction leading
+                to discontinuation of Myfembree was uterine bleeding (1.2%) with onset usually
+                reported within the first 3 months of therapy<sup>1</sup>
+              </li>
+              <li>
+                <strong>Decreased libido:</strong> None of these events were reported as a serious
+                adverse event<sup>2</sup>
+              </li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+      <Row>
         <Col lg={{span: 9, offset: 1}}>
           {/* <div className='footnote-container'>
             <h6>
