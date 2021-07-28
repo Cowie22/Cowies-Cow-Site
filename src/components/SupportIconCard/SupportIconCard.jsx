ccc6094db3fb15b478cb908db478d5a4fc4883f9
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-07-27 18:17:35 -0700

    Begins desktop for new support pages

--- src/components/SupportIconCard/SupportIconCard.jsx
+++ src/components/SupportIconCard/SupportIconCard.jsx
@@ -5,7 +5,7 @@ const SupportIconCard = (props) => {
   const { data } = props;
   const { title, content, icon } = data[0];
   return (
-    <Col lg={4}>
+    <Col lg={6}>
       <div className='support-card-container'>
         <Row>
           <Col lg={{span: 12, offset: 0}} xs={{span: 10, offset: 1}}>
--- src/components/SupportIconCard/SupportIconCardData.js
+++ src/components/SupportIconCard/SupportIconCardData.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import paperIcon from '../../assets/images/paper-icon.svg'
-import shieldIcon from '../../assets/images/shield-and-nike-icon.svg'
-import circleIcon from '../../assets/images/circle-cross-icon.svg'
+import paperIcon from '../../assets/images/paper-and-plus-icon.svg'
+import circleNikeIcon from '../../assets/images/circle-and-nike-transparent-icon.svg'
+import plusInHandIcon from '../../assets/images/plus-in-hand-icon.svg'
 import paperMoneyIcon from '../../assets/images/paper-money-icon.svg'
 
 const SupportIconCardData1 = [
   {
     title: 'Benefits Investigation',
-    content: `Support Program staff can help verify the type of medication coverage the patient has, including
-    formulary status and cost-sharing information`,
+    content: `Support Program staff can help verify the type of medication coverage the patient has,
+    including formulary status and cost-sharing information`,
     icon: paperIcon,
   }
 ]
@@ -18,7 +18,7 @@ const SupportIconCardData2 = [
   {
     title: 'Prior Authorization',
     content: `Electronic prior authorization or assistance with standard fax submissions is available`,
-    icon: shieldIcon,
+    icon: circleNikeIcon,
   }
 ]
 
@@ -26,7 +26,7 @@ const SupportIconCardData3 = [
   {
     title: 'Appeals',
     content: `Support is available in cases of coverage requirements or denials`,
-    icon: circleIcon,
+    icon: plusInHandIcon,
   }
 ]
 
@@ -35,7 +35,7 @@ const SupportIconCardData4 = [
     title: 'Copay Assistance Program*',
     content:
     `Eligible patients with commercial insurance may pay as little as $5 per each monthly
-    prescription or $15 for a 90-day prescription, with a $2000 annual cap`,
+    prescription or $15 for a 90-day prescription, with a $2,000 annual cap`,
     icon: paperMoneyIcon,
   }
 ]
