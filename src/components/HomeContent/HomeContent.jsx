db0dd7d933584985542e26c1c73ceed1e85b6ae3
Ryan Cowie <rcowie@na-sfo-mws0055-269806.local>
2021-08-06 12:05:29 -0700

    Updates per tickets

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -171,7 +171,7 @@ const HomeContent = (props) => {
             <Col>
               <div>
                 <h6>
-                  FDA = US Food and Drug Administration; MBL = menstrual blood loss.
+                  FDA = US Food and Drug Administration.
                 </h6>
               </div>
             </Col>
