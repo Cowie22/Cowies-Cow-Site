33c068511126c33cb3b2083512352e5d4f9d3205
Ryan Cowie <rcowie@na-sfo-mws3217.na.huntsworthhealth.net>
2021-08-12 14:47:11 -0700

    Updates to icon

--- src/components/MyfembreeSupportProgramTabs/Enrollment/Enrollment.jsx
+++ src/components/MyfembreeSupportProgramTabs/Enrollment/Enrollment.jsx
@@ -64,7 +64,7 @@ const Enrollment = (props) => {
             content={
               <div className='enrolment-purple-line-card-content-container'>
                 <Row>
-                  <Col lg={{span: 2, offset: 5}}>
+                  <Col lg={{span: 2, offset: 5}} xs={{span: 6, offset: 3}}>
                     <img src={personSupportIcon} alt='' />
                   </Col>
                 </Row>
