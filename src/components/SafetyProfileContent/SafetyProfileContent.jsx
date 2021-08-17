fd5a609020f76077ba3077000b0d87fefc55c3d8
Ryan Cowie <rcowie@na-sfo-mws3217.attlocal.net>
2021-08-17 13:59:12 -0700

    Updates per ticket for bug

--- src/components/SafetyProfileContent/SafetyProfileContent.jsx
+++ src/components/SafetyProfileContent/SafetyProfileContent.jsx
@@ -156,7 +156,20 @@ const SafetyProfileContent = (props) => {
         </Container>
       </section>
       {
-        currentTopTab !== 1 ?
+        currentTopTab === 1 ?
+        <section className='safety-lower-container'>
+          <Container>
+            <Row>
+              <Col lg={{span: 10, offset: 1}}>
+                <h2 className='white'>
+                  In the LIBERTY open-label extension study, the most commonly reported adverse events were similar
+                  to those in LIBERTY 1 and 2<sup>1</sup>
+                </h2>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        : currentTopTab !== 1 ?
         <>
         </>
         :
