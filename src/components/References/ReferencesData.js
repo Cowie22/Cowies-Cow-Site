6f36670eb357c5207014d92b2ddedc7839420154
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-11-04 07:32:46 -0700

    Adds more references

--- src/components/References/ReferencesData.js
+++ src/components/References/ReferencesData.js
@@ -1,13 +1,118 @@
-import React from 'react'
+import React from "react"
 
 const ReferencesData = {
-  1: <>Zhang L, Huang Y, Hong S, et al. Gemcitabine plus cisplatin versus fluorouracil plus       cisplatin in recurrent or metastatic nasopharyngeal carcinoma: a multicentre, randomised, open-label, phase 3 trial. <em>Lancet</em> 2016;388:1883–92.</>,
-  2: <>Guo T, Qualliotine JR, Ha PK, et al. Surgical salvage improves overall survival for patients with HPV-positive and HPV-negative recurrent locoregional and distant metastatic oropharyngeal cancer. <em>Caner</em>. 2015;121:1977-84.</>,
-  3: <>Naruse T, Yanamoto S, Matsushita Y, et al. Cetuximab for the treatment of locally advanced and recurrent/metastatic oral cancer: an investigation of distant metastasis. <em>Mol Clin Oncol</em>. 2016;5:246-252.</>,
-  4: <>Zhang Q, Wang H, Zhao Q, et al. Evaluation of risk factors for laryngeal squamous cell carcinoma: a single-center retrospective study. <em>Front Oncol</em>. 2021;11:606010.</>,
-  5: <>Referenced with permission from the NCCN Clinical Practice Guidelines in Oncology (NCCN Guidelines®) for Head and Neck Cancers (v3.2021). © National Comprehensive Cancer Network, Inc. 2021. All rights reserved. Accessed August 3, 2021. To view the most recent and complete version of the guidelines, go online to NCCN.org. NCCN makes no warranties of any kind whatsoever regarding their content, use, or application and disclaims any responsibility for their applications or use in any way.</>,
-  6: <>Kuo D-Y, Chang M-H, Wang S-Y, Hsieh P-Y, Shueng P-W. Unusual axillary metastasis of recurrent nasopharyngeal cancer: a case report. <em>Medicine</em> 2017; 96: p e6854.</>,
-  7: <>Wang Y, Zhang Y, Ma S. Racial differences in nasopharyngeal carcinoma in the United States. <em>Cancer Epidemiol</em>Cancer Epidemiol. 2013 December; 37: 793-802.</>,
+  1: (
+    <>
+      Zhang L, Huang Y, Hong S, et al. Gemcitabine plus cisplatin versus
+      fluorouracil plus cisplatin in recurrent or metastatic nasopharyngeal
+      carcinoma: a multicentre, randomised, open-label, phase 3 trial.{" "}
+      <em>Lancet</em> 2016;388:1883–92.
+    </>
+  ),
+  2: (
+    <>
+      Guo T, Qualliotine JR, Ha PK, et al. Surgical salvage improves overall
+      survival for patients with HPV-positive and HPV-negative recurrent
+      locoregional and distant metastatic oropharyngeal cancer. <em>Caner</em>.
+      2015;121:1977-84.
+    </>
+  ),
+  3: (
+    <>
+      Naruse T, Yanamoto S, Matsushita Y, et al. Cetuximab for the treatment of
+      locally advanced and recurrent/metastatic oral cancer: an investigation of
+      distant metastasis. <em>Mol Clin Oncol</em>. 2016;5:246-252.
+    </>
+  ),
+  4: (
+    <>
+      Zhang Q, Wang H, Zhao Q, et al. Evaluation of risk factors for laryngeal
+      squamous cell carcinoma: a single-center retrospective study.{" "}
+      <em>Front Oncol</em>. 2021;11:606010.
+    </>
+  ),
+  5: (
+    <>
+      Referenced with permission from the NCCN Clinical Practice Guidelines in
+      Oncology (NCCN Guidelines®) for Head and Neck Cancers (v3.2021). ©
+      National Comprehensive Cancer Network, Inc. 2021. All rights reserved.
+      Accessed August 3, 2021. To view the most recent and complete version of
+      the guidelines, go online to NCCN.org. NCCN makes no warranties of any
+      kind whatsoever regarding their content, use, or application and disclaims
+      any responsibility for their applications or use in any way.
+    </>
+  ),
+  6: (
+    <>
+      Kuo D-Y, Chang M-H, Wang S-Y, Hsieh P-Y, Shueng P-W. Unusual axillary
+      metastasis of recurrent nasopharyngeal cancer: a case report.{" "}
+      <em>Medicine</em> 2017; 96: p e6854.
+    </>
+  ),
+  7: (
+    <>
+      Wang Y, Zhang Y, Ma S. Racial differences in nasopharyngeal carcinoma in
+      the United States. <em>Cancer Epidemiol</em>Cancer Epidemiol. 2013
+      December; 37: 793-802.
+    </>
+  ),
+  8: (
+    <>
+      American Cancer Society website. Accessed August 13, 2021.
+      https://www.cancer.org/cancer/nasopharyngeal-cancer.html.
+    </>
+  ),
+  9: (
+    <>
+      Chang ET, Adami H-O. The enigmatic epidemiology of nasopharyngeal
+      carcinoma. <em>Cancer Epidemiol Biomarkers Prev</em>. 2006;15:1765-1777.
+    </>
+  ),
+  10: (
+    <>
+      Lo EJ, Bell D, Woo JS, et al. Human papillomavirus and WHO type I
+      nasopharyngeal carcinoma. <em>Laryngoscope</em>. 2010
+      October;120:1990–1997.
+    </>
+  ),
+  11: (
+    <>
+      National Cancer Institute website. Accessed August 13, 2021.
+      https://www.cancer.gov/types/head-and-neck/hp/adult/nasopharyngeal-treatment-pdq.
+    </>
+  ),
+  12: (
+    <>
+      Argirion I, Zarins KR, Ruterbusch JJ, et al. Increasing incidence of
+      Epstein-Barr virus–related nasopharyngeal carcinoma in the United States.{" "}
+      <em>Cancer</em>. 2020;126:121-130.
+    </>
+  ),
+  13: (
+    <>
+      Wang Y, Zhang Y, Ma S. Racial differences in nasopharyngeal carcinoma in
+      the United States. <em>Cancer Epidemiol</em>. 2013 December; 37: 793-802.
+    </>
+  ),
+  14: (
+    <>
+      Wu Z-X, Xiang L, Rong J-F, He H-L, Li D. Nasopharyngeal carcinoma with
+      headaches as the main symptom: a potential diagnostic pitfall.{" "}
+      <em>J Cancer Res Ther</em>. Jan-Mar 2016;12:209-214.
+    </>
+  ),
+  15: (
+    <>
+      Tabuchi K, Nakayama M, Nishimura B, Hayashi K, Hara A. Early detection of
+      nasopharyngeal carcinoma. <em>Int J Otolaryngol</em>. 2011;2011:638058.
+    </>
+  ),
+  16: (
+    <>
+      Mayo Clinic website. Accessed August 9, 2021.
+      https://www.mayoclinic.org/diseases-conditions/nasopharyngeal-carcinoma/symptoms-causes/syc-20375529.
+    </>
+  ),
 }
 
 export default ReferencesData
