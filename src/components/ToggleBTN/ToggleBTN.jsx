86ffbf82ea36591669086b471f56ee9e1fce157d
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-11-04 19:51:32 -0700

    Adds more mobile styles and functionality

--- src/components/ToggleBTN/ToggleBTN.jsx
+++ src/components/ToggleBTN/ToggleBTN.jsx
@@ -11,18 +11,18 @@ const ToggleBTN = (props) => {
   } = state;
 
   return (
-    <Col lg={{span: 6, offset: 3}}>
+    <Col lg={{span: 6, offset: 3}} xs={{span: 12, offset: 0}}>
       <div className='toggle-btn-container' onClick={() => handleSlider(!slider)}>
         <div className={slider ? 'slider-btn right' : 'slider-btn left'}>
 
         </div>
         <Row>
-          <Col lg={{span: 6, offset: 0}}>
+          <Col lg={{span: 6, offset: 0}} xs={{span: 6, offset: 0}}> 
             <h4 className={!slider ? 'text-center dark-grey' : 'text-center white'}>
               Non-Hispanic White <br /> (n=4,013)
             </h4>
           </Col>
-          <Col lg={{span: 6, offset: 0}}>
+          <Col lg={{span: 6, offset: 0}} xs={{span: 6, offset: 0}}> 
             <h4 className={slider ? 'text-center dark-grey' : 'text-center white'}>
               Asian <br /> (n=3,381)
             </h4>
