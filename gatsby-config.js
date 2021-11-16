15f736cf2939009891f4f193bb01c54a29dc883f
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-11-16 04:21:08 -0800

    Runs optimization tests and saves for the night

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-minify`,
+    `gatsby-plugin-minify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -21,12 +21,12 @@ module.exports = {
         defaultDataLayer: { platform: "gatsby" },
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-brotli',
-    //   options: {
-    //     extensions: ['css', 'html', 'js', 'svg', 'jsx', 'webp', 'scss', 'png', 'jpg']
-    //   }
-    // },
+    {
+      resolve: 'gatsby-plugin-brotli',
+      options: {
+        extensions: ['css', 'html', 'js', 'svg', 'jsx', 'webp', 'scss', 'png', 'jpg']
+      }
+    },
     {
       resolve: `gatsby-plugin-google-fonts-with-attributes`,
       options: {
