eb46be765ff451a3d7b92f1b7331088bdcf1a48d
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-12-09 02:24:22 -0800

    More updates to redirects

--- redirectData.js
+++ redirectData.js
@@ -4,6 +4,10 @@ const redirectData = [
     to: '/?utm_source=nejm&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   },
   {
+    from: '/nejm/',
+    to: '/?utm_source=nejm&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
     from: '/jco',
     to: '/?utm_source=jco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   },
@@ -16,7 +20,15 @@ const redirectData = [
     to: '/?utm_source=asco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   },
   {
+    from: '/asco/',
+    to: '/?utm_source=asco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
     from: '/lancet',
+    to: '/?utm_source=lancet&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
+    from: '/lancet/',
     to: '/?utm_source=lancet&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   }
 ]
