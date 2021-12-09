6e35c00a9e579033a470a9444fe6437420e2cd9d
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-12-09 01:34:44 -0800

    Removes bg image hover change and adds redirects

--- redirectData.js
+++ redirectData.js
@@ -1,7 +1,19 @@
 const redirectData = [
   {
-    from: '',
-    to: '',
+    from: '/nejm',
+    to: '/?utm_source=nejm&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
+    from: '/jco',
+    to: '/?utm_source=jco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
+    from: '/asco',
+    to: '/?utm_source=asco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
+    from: '/lancet',
+    to: '/?utm_source=lancet&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   },
 ]
 
