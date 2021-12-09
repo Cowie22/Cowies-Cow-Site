000f0682114db1abf2f018bc026e10be2eeaa0a0
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-12-09 02:18:42 -0800

    More modifications to config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const { redirectData } = require('./redirectData.js')
 
-exports.onCreateWebpackConfig = function onCreateWebpackConfig({ actions, stage, loaders }) {
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'develop') {
     actions.setWebpackConfig({
       module: {
@@ -15,15 +15,21 @@ exports.onCreateWebpackConfig = function onCreateWebpackConfig({ actions, stage,
       }
     })
   }
+  actions.setWebpackConfig({
+    node: {
+      fs: "empty",
+      child_process: "empty",
+    },
+  })
 }
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createRedirect } = actions;
+  const { createRedirect } = actions; //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
   redirectData.map((redirect, i) => {
     const { from, to } = redirect;
     return (
       createRedirect({
-        fromPath: from,
+        fromPath: from.toLowerCase(),
         toPath: to,
         isPermanent: true,
         statusCode: 301,
@@ -32,4 +38,19 @@ exports.createPages = ({ graphql, actions }) => {
       })
     )
   })
+  // if (typeof window !== "undefined") {
+  //   let originalUrl = window.location.href;
+  //   let lowerCaseUrl = originalUrl.toLowerCase();
+  //   console.log('OG', originalUrl)
+  //   if (originalUrl !== lowerCaseUrl) {
+  //     createRedirect({
+  //       fromPath: originalUrl,
+  //       toPath: lowerCaseUrl,
+  //       isPermanent: true,
+  //       statusCode: 301,
+  //       redirectInBrowser: true,
+  //       force: true,
+  //     })
+  //   }
+  // }
 }
--- redirectData.js
+++ redirectData.js
@@ -8,13 +8,17 @@ const redirectData = [
     to: '/?utm_source=jco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   },
   {
+    from: '/jco/',
+    to: '/?utm_source=jco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
+  },
+  {
     from: '/asco',
     to: '/?utm_source=asco&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
   },
   {
     from: '/lancet',
     to: '/?utm_source=lancet&utm_medium=print&utm_campaign=dse&utm_id=none&utm_term=none&utm_content=none',
-  },
+  }
 ]
 
 module.exports ={
