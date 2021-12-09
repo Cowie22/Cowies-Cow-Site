e8fd2ca96dc86485e8bbd80bafabb3a4577a2779
Ryan Cowie <rcowie@na-sfo-mws3217.local>
2021-12-09 02:30:35 -0800

    Adds dummy pages

---
+++ src/pages/jco.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from 'react'
+import Layout from '../components/Layout'
+
+const Jco = props => {
+  return (
+    <Layout canonicalURL='' title='' pageTitle='' description=''>
+    
+    </Layout>
+  )
+}
+
+export default Jco
